216b97c91b41c9e21743f3562e70584ab4b7f519
Dale Fugier <dale@mcneel.com>
2021-04-20 09:41:54 -0700

    Assembly info update

--- CsPotrace.cs
+++ CsPotrace.cs
@@ -6,6 +6,7 @@ using System.Linq;
 
 // Copyright (C) 2001-2016 Peter Selinger
 // Copyright (C) 2009-2016 Wolfgang Nagl
+// Copyright (C) 2020-2021 Dale Fugier
 
 // This program is free software; you can redistribute it and/or modify  it under the terms of the GNU General Public License as published by  the Free Software Foundation; either version 2 of the License, or (at  your option) any later version.
 // This program is distributed in the hope that it will be useful, but  WITHOUT ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU  General Public License for more details.
